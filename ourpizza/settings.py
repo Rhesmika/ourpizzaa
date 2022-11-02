193dd30f4c0237284d67c3ea1e1ab9958f6c7815
Rhesmika <Rhesmika@gmail.com>
2022-11-02 22:11:48 +0000

    update host url

--- ourpizza/settings.py
+++ ourpizza/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ourpizza.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["our-pizza.herokuapp.com", "localhost"]
 
 
 # Application definition
