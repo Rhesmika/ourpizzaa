01f0048c5c3e16611741cad82fe614c5c21b8218
Rhesmika <Rhesmika@gmail.com>
2022-11-07 21:14:09 +0000

    Create booking model and add to admin access

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Booking
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Booking)
+class bookingAdmin(SummernoteModelAdmin):
+    summernote_fields = ('notes')
--- book/models.py
+++ book/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+REQUEST_STATUS = ((0, 'Pending'), (1, 'Approved'), (3, 'Denied'))
+
+
+class Booking(models.Model):
+    booking_date = models.DateField()
+    party_of = models.IntegerField(
+        default=2,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+     )
+    notes = models.CharField(max_length=500)
+    reference = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    status = models.IntegerField(choices=REQUEST_STATUS, default=0)
+
+
+class Meta:
+    ordering = ['-created_on']
+
+
+def __str__(self):
+    return self.reference
